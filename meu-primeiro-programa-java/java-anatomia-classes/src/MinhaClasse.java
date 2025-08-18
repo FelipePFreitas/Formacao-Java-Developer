da6cfc93ab50bbcdf8eb5ea11bc9c80da0f50929
Felipe Freitas <felipe.freitas1991@outlook.com>
2025-08-18 19:07:44 -0300

    Anatomia das Classes - 2

--- meu-primeiro-programa-java/java-anatomia-classes/src/MinhaClasse.java
+++ meu-primeiro-programa-java/java-anatomia-classes/src/MinhaClasse.java
@@ -1,7 +1,21 @@
 public class MinhaClasse {
 
      public static void main(String [] args) {
-        System.out.println("Olá meu povo, sejam bem-vindos !");
+
+       // Declaração inválida de variáveis
+
+       int numero&um = 1; //Os unicos simbolos parmitidos são _ e $
+       int 1numero = 1; // Uma variavel não pode começar com numero
+       int numero um = 1; //Não pode ter espaço no nome da variável
+       int long = 1; //long faz parte das palavras reservadas da linguagem JAVA
+
+       // Declaração válida de variáveis
+
+       int numero$um = 1;
+       int numero1 = 1;
+       int numeroUm = 1;
+       int longo = 1;
+
 
     }
 }
