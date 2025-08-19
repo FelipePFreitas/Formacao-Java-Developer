87f7bd7832427387de442f829c07fdd987d0318b
Felipe Freitas <felipe.freitas1991@outlook.com>
2025-08-19 18:48:46 -0300

    Anatomia das Classes - 3

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,41 +1,17 @@
 public class MinhaClasse {
 
      public static void main(String [] args) {
+      String primeiroNome = "Felipe";
+      String segundoNome = "Freitas";
+      String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+      System.out.println(nomeCompleto);
 
-       // Declaração inválida de variáveis
-
-       int numero&um = 1; //Os unicos simbolos parmitidos são _ e $
-       int 1numero = 1; // Uma variavel não pode começar com numero
-       int numero um = 1; //Não pode ter espaço no nome da variável
-       int long = 1; //long faz parte das palavras reservadas da linguagem JAVA
-
-       // Declaração válida de variáveis
-
-       int numero$um = 1;
-       int numero1 = 1;
-       int numeroUm = 1;
-       int longo = 1;
+    
      }
 
-     public static void declaracaoVariaveis(String [] args) {
+     public static String nomeCompleto(String primeiroNome, String segundoNome){
 
-      //Estrutura
-
-      Tipo NomeBemDefinido = Atribuicao ("opcional em alguns casos");
-
-      // exemplo
-
-      int idade = 23;
-      double altura = 1.62;
-      spike; //Observe aqui a variavel spike não tem valor
-
-
-      String meuNome = "Felipe";
-      
-      int anoFabricacao = 2025;
-
-      boolean verdadeiraOuFalsa = true or false ;
-
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
 
      }
 }
