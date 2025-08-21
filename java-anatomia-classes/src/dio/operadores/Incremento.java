b479276b7ea4ad16b3e9144924d417518ca1a349
Felipe Freitas <felipe.freitas1991@outlook.com>
2025-08-21 08:51:55 -0300

    Operadores - 4

---
+++ java-anatomia-classes/src/dio/operadores/Incremento.java
@@ -0,0 +1,18 @@
+public class Incremento{
+
+public static void main(String[] args) {
+    
+    int numero = 5;
+    //repetição
+
+    System.out.println(++numero);
+    System.out.println(numero);
+
+    boolean variavel = true;
+
+    System.out.println(!variavel);
+
+
+}
+
+}
