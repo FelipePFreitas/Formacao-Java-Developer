0fbd741833164ccf1416d51b24ab3de19bef4699
Felipe Freitas <felipe.freitas1991@outlook.com>
2025-08-28 17:25:54 -0300

    Operadores - 5

---
+++ java-anatomia-classes/src/dio/operadores/Ternario.java
@@ -0,0 +1,27 @@
+public class Ternario {
+
+    public static void main(String[] args) {
+        
+        int a,b;
+
+        a=5;
+        b=6;
+
+        String resultado = a==b ? "Verdadeiro" : "Falso";
+
+        System.out.println(resultado);
+
+        // EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTRA IF/ELSE
+
+        // if(a==b)
+        //     resultado = "Verdadeiro";
+        // else
+        //     resultado = "falso";
+        //  System.out.println(resultado);   
+            
+
+
+
+    }
+
+}
