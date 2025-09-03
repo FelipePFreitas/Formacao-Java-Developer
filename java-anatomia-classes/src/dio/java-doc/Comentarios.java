be02be4eb68631d879aeefaa09aceef024e424f2
Felipe Freitas <felipe.freitas1991@outlook.com>
2025-09-03 13:04:17 -0300

    Java Doc - 2

---
+++ java-anatomia-classes/src/dio/java-doc/Comentarios.java
@@ -0,0 +1,26 @@
+
+public class Comentarios {
+
+    public static void main(String[] args) {
+
+        // Olá sou um comentário em uma única linha
+
+        /* Olá, 
+        Eu sou um comentário 
+        que possuo ser mais detalhado 
+        quando necessário 
+        */
+    
+    }
+
+    /**
+     * Esta duas estrelinha acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nivel de documentação.
+     */
+    public void metodo(){
+
+    }
+
+}
