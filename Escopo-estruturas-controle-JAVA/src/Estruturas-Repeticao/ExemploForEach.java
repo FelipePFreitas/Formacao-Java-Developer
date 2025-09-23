2e248caef5c1692597854efc757b402994c1e98c
Felipe Freitas <felipe.freitas1991@outlook.com>
2025-09-23 15:36:19 -0300

    Exemplo For Each

---
+++ Escopo-estruturas-controle-JAVA/src/Estruturas-Repeticao/ExemploForEach.java
@@ -0,0 +1,15 @@
+
+public class ExemploForEach {
+
+    public static void main(String[] args) {
+
+        // em arrays o indice de elementos inicia em ZERO
+        String alunos[] = {"Felipe", "Jonas", "Julia", "Marcos"};
+
+        for (String aluno : alunos) {
+            System.out.println("Nome do aluno é: " + aluno);
+        }
+
+    }
+
+}
